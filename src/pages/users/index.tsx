5e2163c2f5a95d180087401ed1622298dbafc993
Rafael Yamada <rafaelyamada321@gmail.com>
2022-01-11 04:22:34 -0400

    fix createdAt

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -5,11 +5,10 @@ import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
-import { getUsers, useUsers } from "../../services/hooks/useUsers";
+import { useUsers } from "../../services/hooks/useUsers";
 import { useState } from "react";
 import { queryClient } from "../../services/queryClient";
 import { api } from "../../services/api";
-import { GetServerSideProps } from "next";
 
 export default function UserList() {
   const [page, setPage] = useState(1);
